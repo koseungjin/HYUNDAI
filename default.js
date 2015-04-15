2d41f9e0e48644b866e7501ac1d60c872bb8150f
Charles Kwon <youngcheol.kwon@gmail.com>
2015-04-15 19:54:21 +0900

    날짜 추가해야함
    날짜 추가해야함

--- default.js
+++ default.js
@@ -33,10 +33,19 @@ function showWindowSize() {
 //});
 
 jQuery(function($){
+    /*메인 메뉴*/
     var $mainGnb = $(".main_gnb ul>li>a");
     $mainGnb.on("click", function(){
         $(this).addClass("active")
             .parent("li").siblings().children("a")
                 .removeClass("active");
+    });
+    /*콘텐츠 메뉴*/
+    $('#content_lnb').on('click', 'li a', function(){
+        var $this = $(this);
+        $($this.attr('href'))
+                .addClass('active')
+            .siblings()
+                .removeClass('active'); 
     });
 });
